bd9a82b7551ce9e23aa432e315d92ef52376983a
pat <patlanzm@gmail.com>
2025-12-14 14:11:25 +0800

    Add files via upload

--- schema_v1.sql
+++ schema_v1.sql
@@ -1,1 +1,7 @@
 
+CREATE TABLE users (
+    username VARCHAR(50) PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('Admin','Staff','Resident') DEFAULT 'Resident'
+);
