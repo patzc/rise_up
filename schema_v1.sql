0712d7a4895c1381225082f4e4ef9f236b9db157
pat <patlanzm@gmail.com>
2025-12-17 18:09:45 +0800

    Update schema_v1.sql

--- schema_v1.sql
+++ schema_v1.sql
@@ -1,7 +1,8 @@
-
 CREATE TABLE users (
-    username VARCHAR(50) PRIMARY KEY,
-    full_name VARCHAR(255) NOT NULL,
+    username VARCHAR(100) PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role ENUM('Admin','Staff','Resident') DEFAULT 'Resident'
+    role VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
