21b2be923eff6b1391bbcabca3ef59e55533bf51
pat <patlanzm@gmail.com>
2025-12-17 18:10:03 +0800

    schema_v2.sql

--- schema_v2.sql
+++ schema_v2.sql
@@ -1,11 +1,9 @@
-
 CREATE TABLE beneficiaries (
-  beneficiary_id INT NOT NULL AUTO_INCREMENT,
-  full_name VARCHAR(255) NOT NULL,
-  age INT,
-  gender ENUM('Male','Female','Other'),
-  contact VARCHAR(20),
-  address VARCHAR(255),
-  income_level VARCHAR(50),
-  PRIMARY KEY (beneficiary_id)
+    beneficiary_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    contact VARCHAR(20),
+    income_level VARCHAR(50),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
